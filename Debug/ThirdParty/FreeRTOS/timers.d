3753402776cbf30980469dd166a1f406321ba558
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2025-03-02 00:05:46 +0700

    frist integrate freeRTOS to STM32 project

---
+++ Debug/ThirdParty/FreeRTOS/timers.d
@@ -0,0 +1,37 @@
+ThirdParty/FreeRTOS/timers.o: ../ThirdParty/FreeRTOS/timers.c \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/queue.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h \
+ E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/timers.h
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/projdefs.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/portable.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/list.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/queue.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/task.h:
+
+E:/Hoc/Stm32/RTOS/Workspace/FreeRTOS_STM32/ThirdParty/FreeRTOS/include/timers.h:
