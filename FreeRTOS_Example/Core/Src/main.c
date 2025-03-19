d5aa922dbd72a35f11c90987957b21843872baaa
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2025-03-20 00:11:21 +0700

    integrate SysView s2

--- FreeRTOS_Example/Core/Src/main.c
+++ FreeRTOS_Example/Core/Src/main.c
@@ -6,6 +6,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#define DWT_CTRL    (*(volatile uint32_t*)0xE0001000)
+
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void task1_handler(void* parameters);
@@ -24,7 +26,12 @@ int main(void)
   SystemClock_Config();
 
   MX_GPIO_Init();
+  //Enable the CYCCNT counter.
+  DWT_CTRL |= ( 1 << 0);
 
+  SEGGER_SYSVIEW_Conf();
+
+  SEGGER_SYSVIEW_Start();
   //task creation
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1\n", 2, &task1_handle);
   status = xTaskCreate(task2_handler, "Task-2", 200, "Hello world from Task-2\n", 2, &task2_handle);
